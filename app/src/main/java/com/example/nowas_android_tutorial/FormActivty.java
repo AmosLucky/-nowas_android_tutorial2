b967f60f907e5d8ae07e9915d001804b717e721a
Amos Lucky <aluckya100@gmail.com>
2022-08-24 13:43:35 +0100

    for day 1

---
+++ app/src/main/java/com/example/nowas_android_tutorial/FormActivty.java
@@ -0,0 +1,14 @@
+package com.example.nowas_android_tutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_activty);
+    }
+}
